ff4b85b42c07ab7883ed831f0271aa75e23093db
TengyuSun <tengyu.sun@kcl.ac.uk>
2022-10-02 14:43:22 +0100

    fixedV

--- employee.py
+++ employee.py
@@ -36,16 +36,16 @@ class Employee:
 billie = Employee('Billie', 4000, 0, 0, 0, 0, 0)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie', 0, 100, 25, 0, 0, 0)
+charlie = Employee('Charlie', 0, 25, 100, 0, 0, 0)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Employee('Renee', 3000, 0, 0, 0, 200, 4)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan', 0, 150, 25, 0, 220, 3)
+jan = Employee('Jan', 0, 25, 100, 0, 220, 3)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Employee('Robbie', 2000, 0, 0, 1500, 0, 0)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel', 0, 120, 30, 600, 0, 0)
+ariel = Employee('Ariel', 0, 30, 120, 600, 0, 0)
